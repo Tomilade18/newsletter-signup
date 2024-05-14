d24118e497cb9b29eabee7e92725c62b6a3a6621
Tomilade18 <jboyyumzey@gmail.com>
2024-05-14 15:57:25 +0100

    improved

--- newsletter-signup/src/App.jsx
+++ newsletter-signup/src/App.jsx
@@ -8,9 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className=''>
         <form action="">
           <div className='input-field'> 
+          
+            <h1>Stay updated !</h1>
+            <p className='message'>Join 60000+ product managers receiving monthly updates on:</p>
+           <ul>
+            <li className='list'><img src="./src/assets/icon-list.svg" alt="" />product discovery and building what matters</li>
+            <li className='list'><img src="./src/assets/icon-list.svg" alt="" />measuring to ensure updates are a sucess</li>
+            <li className='list'><img src="./src/assets/icon-list.svg" alt="" />And much more</li>
+           </ul>
             <label htmlFor="">
               Email Address
             </label>
@@ -18,7 +26,7 @@ function App() {
             <button>Subscribe to monthly newsletter</button>
           </div>
           <div className='image'>
-            <img src="./src/assets/Capture.jpg" alt="" />
+            <img src="./src/assets/illustration-sign-up-desktop.svg" alt="" />
           </div>
          
         </form>

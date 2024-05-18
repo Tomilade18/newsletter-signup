273632f16b42f8b60c860e8bfe010692d03594ac
Tomilade18 <jboyyumzey@gmail.com>
2024-05-19 00:26:26 +0100

    unsolved issue with routing

--- newsletter-signup/src/App.jsx
+++ newsletter-signup/src/App.jsx
@@ -1,1 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Success from './success'
 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=''>
+        <form action="">
+          <div className='input-field'> 
+          
+            <h1>Stay updated !</h1>
+            <p className='message'>Join 60000+ product managers receiving monthly updates on:</p>
+           <ul>
+            <li className='list'><img src="./src/assets/icon-list.svg" alt="" />product discovery and building what matters</li>
+            <li className='list'><img src="./src/assets/icon-list.svg" alt="" />measuring to ensure updates are a sucess</li>
+            <li className='list'><img src="./src/assets/icon-list.svg" alt="" />And much more</li>
+           </ul>
+            <label htmlFor="">
+              Email address
+            </label>
+            <input type="email" placeholder='email@company.com'/>
+            <button>Subscribe to monthly newsletter</button>
+          </div>
+          <div className='image'>
+            <img src="./src/assets/illustration-sign-up-desktop.svg" alt="" />
+          </div>
+         
+        </form>
+      </div>
+    </>
+   
+  )
+}
+
+export default App

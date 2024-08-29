eb249d091734830e3ea412cf0b0cddf43b9607d4
Tomilade18 <jboyyumzey@gmail.com>
2024-08-29 12:28:34 +0100

    new

--- newsletter-signup/src/success.jsx
+++ newsletter-signup/src/success.jsx
@@ -2,7 +2,7 @@ export default function Success() {
     
     return( 
         <div className="success">
-        <img src="./icon-success.svg" alt="" />
+        <img src="./icon-success.svg" className="suc-img" alt="" />
         <h1 className="suc-head">Thanks for subscribing</h1>
         <p className="suc-text">A confirmation email has been sent to ash@loremcompany.com. Please open it and click the button inside to confirm your subscription</p>
         <button className="dismiss-btn"><a href="/">Dismiss Message</a></button>
